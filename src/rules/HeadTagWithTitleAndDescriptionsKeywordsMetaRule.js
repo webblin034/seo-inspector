b0f2e9e237c28c98a6890b9541514eff20b0a3f9
webblin <webblin@oath.com>
2017-12-03 22:19:09 +0800

    fix by standard lint

--- src/rules/HeadTagWithTitleAndDescriptionsKeywordsMetaRule.js
+++ src/rules/HeadTagWithTitleAndDescriptionsKeywordsMetaRule.js
@@ -1,27 +1,25 @@
-'use strict';
+'use strict'
 
-import BaseRule from './BaseRule';
+import BaseRule from './BaseRule'
 
 class HeadTagWithTitleAndDescriptionsKeywordsMetaRule extends BaseRule {
-
-    check(dom) {
-        if (this.options.enabled === 0) {
-            return;
-        }
-	let report = [];
-        if (!dom.window.document.title) {
-            report.push('This HTML without <title> tag');
-        }
-        const metaList = ['descriptions', 'keywords'];
-        metaList.forEach(function(meta) {
-            let element = dom.window.document.querySelector('head > meta[name=' + meta + ']');
-            if (!element || (element && !element.getAttribute('content'))) {
-                report.push('This HTML without <meta name="' + meta + '"> tag');
-            }
-        });
-	return report.join("\n");
+  check (dom) {
+    if (this.options.enabled === 0) {
+      return
     }
-
+    let report = []
+    if (!dom.window.document.title) {
+      report.push('This HTML without <title> tag')
+    }
+    const metaList = ['descriptions', 'keywords']
+    metaList.forEach((meta) => {
+      let element = dom.window.document.querySelector('head > meta[name=' + meta + ']')
+      if (!element || (element && !element.getAttribute('content'))) {
+        report.push('This HTML without <meta name="' + meta + '"> tag')
+      }
+    })
+    return report.join('\n')
+  }
 }
 
-export default HeadTagWithTitleAndDescriptionsKeywordsMetaRule;
+export default HeadTagWithTitleAndDescriptionsKeywordsMetaRule
