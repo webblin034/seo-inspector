3ca60188e9e53172d93a408bdeaa18ade77ef5d6
webblin <webblin@oath.com>
2017-12-03 22:15:24 +0800

    fix by standard lint check.

--- src/rules/ImgTagWithAltAttritubeRule.js
+++ src/rules/ImgTagWithAltAttritubeRule.js
@@ -1,27 +1,25 @@
-'use strict';
+'use strict'
 
-import BaseRule from './BaseRule';
+import BaseRule from './BaseRule'
 
 class ImgTagWithAltAttritubeRule extends BaseRule {
-
-    check(dom) {
-        if (this.options.enabled === 0) {
-            return;
-        }
-        let report = '';
-        let count = 0;
-        const elements = dom.window.document.querySelectorAll('img');
-        elements.forEach((element) => {
-            if (!element.alt) { 
-                count++;
-            }
-        });
-        if (count > 0) {
-            report += 'There are ' + count + ' <img> tag without alt attribute'; 
-        }
-        return report;
+  check (dom) {
+    if (this.options.enabled === 0) {
+      return
     }
-
+    let report = ''
+    let count = 0
+    const elements = dom.window.document.querySelectorAll('img')
+    elements.forEach((element) => {
+      if (!element.alt) {
+        count++
+      }
+    })
+    if (count > 0) {
+      report += 'There are ' + count + ' <img> tag without alt attribute'
+    }
+    return report
+  }
 }
 
-export default ImgTagWithAltAttritubeRule;
+export default ImgTagWithAltAttritubeRule
