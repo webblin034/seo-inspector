f57d84680483da6fc48b26967da70506c6f29dea
webblin <webblin@oath.com>
2017-12-03 22:20:57 +0800

    fix by standard lint

--- src/rules/NoTooManyStrongTagsRule.js
+++ src/rules/NoTooManyStrongTagsRule.js
@@ -1,21 +1,19 @@
-'use strict';
+'use strict'
 
-import BaseRule from './BaseRule';
+import BaseRule from './BaseRule'
 
 class NoTooManyStrongTagsRule extends BaseRule {
-
-    check(dom) {
-        if (this.options.enabled === 0) {
-            return;
-        }
-        let report = '';
-        const elements = dom.window.document.querySelectorAll('strong');
-        if (elements && elements.length > this.options.threshold) {
-            report += 'This HTML have more than ' + this.options.threshold + ' <strong> tags';
-        }
-        return report;
+  check (dom) {
+    if (this.options.enabled === 0) {
+      return
     }
-
+    let report = ''
+    const elements = dom.window.document.querySelectorAll('strong')
+    if (elements && elements.length > this.options.threshold) {
+      report += 'This HTML have more than ' + this.options.threshold + ' <strong> tags'
+    }
+    return report
+  }
 }
 
-export default NoTooManyStrongTagsRule;
+export default NoTooManyStrongTagsRule
