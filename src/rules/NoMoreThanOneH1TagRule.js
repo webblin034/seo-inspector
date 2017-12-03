85e7735a82286c7810848d96e1c190b4170266f4
webblin <webblin@oath.com>
2017-12-03 22:20:16 +0800

    fix by standard lint

--- src/rules/NoMoreThanOneH1TagRule.js
+++ src/rules/NoMoreThanOneH1TagRule.js
@@ -1,21 +1,19 @@
-'use strict';
+'use strict'
 
-import BaseRule from './BaseRule';
+import BaseRule from './BaseRule'
 
 class NoMoreThanOneH1Tag extends BaseRule {
-
-    check(dom) {
-        if (this.options.enabled === 0) {
-            return;
-        }
-        let report = '';
-        const elements = dom.window.document.querySelectorAll('h1');
-        if (elements && elements.length > 1) {
-            report += 'This HTML have more than one <h1> tag';
-        }
-        return report;
+  check (dom) {
+    if (this.options.enabled === 0) {
+      return
     }
-
+    let report = ''
+    const elements = dom.window.document.querySelectorAll('h1')
+    if (elements && elements.length > 1) {
+      report += 'This HTML have more than one <h1> tag'
+    }
+    return report
+  }
 }
 
-export default NoMoreThanOneH1Tag;
+export default NoMoreThanOneH1Tag
